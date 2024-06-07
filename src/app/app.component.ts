a09305ea4f2b1810dc0dab2b5c5cda3f29b27832
Daniel Penelva <d4n.andrade@gmail.com>
2024-06-06 22:26:30 -0300

    Control Flow - Condicional @if

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { Componente01Component } from "./componente01/componente01.component";
 import { DiretivasComponent } from "./diretivas/diretivas.component";
+import { ControlFlowComponent } from "./control-flow/control-flow.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
-    imports: [CommonModule, RouterOutlet, Componente01Component, DiretivasComponent]
+    imports: [CommonModule, RouterOutlet, Componente01Component, DiretivasComponent, ControlFlowComponent]
 })
 export class AppComponent {
   title = 'demo-angular17';
