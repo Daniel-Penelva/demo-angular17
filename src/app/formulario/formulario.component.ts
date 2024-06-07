7ba23e2bcc1101e5725cbbd1b3df6a2e98898202
Daniel Penelva <d4n.andrade@gmail.com>
2024-06-06 22:46:22 -0300

    criando componente formulario + diretiva [(ngModel)] + importando FormsModule (Parte 1)

---
+++ src/app/formulario/formulario.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-formulario',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './formulario.component.html',
+  styleUrl: './formulario.component.css'
+})
+export class FormularioComponent {
+
+  nome: string = '';
+}
+
+/* Para utilizar formulários em uma aplicação angular vai ser preciso importar o FormsModule. */
