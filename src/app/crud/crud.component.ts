43579e90051cf547d5be8c24ae550cc74337e0c8
Daniel Penelva <d4n.andrade@gmail.com>
2024-06-07 01:57:06 -0300

    Função de cadastro de Pessoa

--- src/app/crud/crud.component.ts
+++ src/app/crud/crud.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Pessoa } from '../model/Pessoa';
 
 @Component({
   selector: 'app-crud',
@@ -11,10 +12,22 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
 })
 export class CrudComponent {
 
+  pessoas: Pessoa[] = [];
+
   formulario = new FormGroup({
     nome: new FormControl('', [Validators.required, Validators.minLength(2)]),
     idade: new FormControl(null, [Validators.required, Validators.min(0), Validators.max(120)]),
     cidade: new FormControl('', [Validators.required, Validators.minLength(3)]),
   });
+
+  // Função de cadastro de pessoa
+  cadastrarPessoa(){
+
+    // cadastrar no vetor
+    this.pessoas.push(this.formulario.value as Pessoa);
+
+    // Visualização via console
+    console.table(this.pessoas);
+  }
   
 }
