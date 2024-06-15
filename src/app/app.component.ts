d64238720e1e116e417270148d3601c9ea5fa03a
Daniel Penelva <d4n.andrade@gmail.com>
2024-06-14 21:44:52 -0300

    Rotas (Route)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { Componente01Component } from "./componente01/componente01.component";
 import { DiretivasComponent } from "./diretivas/diretivas.component";
 import { ControlFlowComponent } from "./control-flow/control-flow.component";
@@ -26,13 +26,15 @@ import { ObservableComponente2Component } from "./observable-componente2/observa
 import { ObservableComponente3Component } from "./observable-componente3/observable-componente3.component";
 import { ObservableComponente4Component } from "./observable-componente4/observable-componente4.component";
 import { ObservableComponente5Component } from "./observable-componente5/observable-componente5.component";
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
-    imports: [CommonModule, RouterOutlet, Componente01Component, DiretivasComponent, ControlFlowComponent, FormularioComponent, FormularioReativoComponent, CrudComponent, ClasseAngularComponent, InterfaceAngularComponent, EnumeradoresAngularComponent, ModeloEntidadesComponent, EventBindingComponent, UserListComponentComponent, ParentComponentComponent, ChildComponentComponent, FilhoComponent, PaiComponent, ComponentePaiComponent, SintaxeEspalhamentoComponent, PipeComponentComponent, PipePersonalizadoComponent, ObservableComponenteComponent, ObservableComponente2Component, ObservableComponente3Component, ObservableComponente4Component, ObservableComponente5Component]
+    imports: [CommonModule, RouterOutlet, Componente01Component, DiretivasComponent, ControlFlowComponent, FormularioComponent, FormularioReativoComponent, CrudComponent, ClasseAngularComponent, InterfaceAngularComponent, EnumeradoresAngularComponent, ModeloEntidadesComponent, EventBindingComponent, UserListComponentComponent, ParentComponentComponent, ChildComponentComponent, FilhoComponent, PaiComponent, ComponentePaiComponent, SintaxeEspalhamentoComponent, PipeComponentComponent, PipePersonalizadoComponent, ObservableComponenteComponent, ObservableComponente2Component, ObservableComponente3Component, ObservableComponente4Component, ObservableComponente5Component, HomeComponent, AboutComponent, RouterModule]
 })
 export class AppComponent {
   title = 'demo-angular17';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'home', component: HomeComponent},
+    {path: 'about', component: AboutComponent},
+    {path: '**', redirectTo: '/home'}            // Rota Coringa: Redireciona para 'home' se nenhuma rota corresponder
+];
+
+
+/*Acessos: 
+ * Testando rota padrão (path: '') - http://localhost:4200/              :. OBS. Nessa rota será visualizado a página Home (http://localhost:4200/home)
+ * Testando rota Home (path: 'home') - http://localhost:4200/home
+ * Testando rota About (path: 'about') - http://localhost:4200/about
+ * Testando rota não correspondente que será redirecionado para Home (path:'**', redirectTo) - http://localhost:4200/teste
+*/
+
+/* Observação: 
+ * {path: '', component: HomeComponent} 
+ * 
+ * Rota raiz para ser exibido por padrão o Home. Ou pode configurar uma rota raiz ('') para não carregar nenhum componente inicialmente.
+*/
